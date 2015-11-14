2a9bbf8b210ca372c721d09ed4d2e53a3a71369a
cokasaki <cokasaki@hmc.edu>
2015-11-13 20:05:12 -0800

    set up some basic stuff

--- main.lua
+++ main.lua
@@ -1,3 +1,26 @@
+-- THE MAIN FILE FOR 5C HACKATHON FALL 2015
+
+
+-- IMPORT ANY NECESSARY FILES/PACKAGES
+gamestate = require 'lib/hump.gamestate'
+
+
+
+-- initialize everything at the start of the game
+function love.load(arg)
+    -- do nothing
+end
+
+
+
+-- update the game through a timestep 'dt'
+function love.update(dt)
+    -- do nothing
+end
+
+
+
+-- draw the game to the screen
 function love.draw()
-    love.graphics.print("hello world", 100, 100)
+    love.graphics.print("hello world",100,100)
 end
