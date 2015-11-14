327b636e83f990538564e7294af786d8855108b7
Robert Linden <robert.linden@aol.com>
2015-11-13 23:21:18 -0800

    Added Constants; drew resources and cards

--- constants.lua
+++ constants.lua
@@ -10,6 +10,8 @@ c =
     P_TWO_HAND = {y = 550},
     P_ONE_RES = {y = 100},
     P_TWO_RES = {y = 500},
+    MAX_CARDS = 6,
+    MAX_RES = 9,
 }
 
 colors =
@@ -18,4 +20,5 @@ colors =
     RED   = {255,0  ,0  ,255},
     P_ONE = {255,0  ,0  ,255},
     P_TWO = {0  ,0  ,255,255},
+    RES = {170, 170, 170, 255},
 }
